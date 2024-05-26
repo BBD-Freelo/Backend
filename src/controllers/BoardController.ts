f5fe2cddc0ff8f9d026148083bf9c174381ffd6c
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-05-26 20:12:32 +0200

    interface updates

--- src/controllers/BoardController.ts
+++ src/controllers/BoardController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { Controller, Get, Post } from "../decorators";
 import { controller, EndpointDefenition } from '../interfaces';
-import { list } from "../interfaces/entities";
+import { List } from "../interfaces/entities";
 import { DBPool } from '../database';
 
 @Controller('/board')
@@ -15,10 +15,10 @@ export class BoardController implements controller {
 
     // Or id?
     @Get('/:name')
-    hello(req: Request, res: Response<list[]>) {
+    hello(req: Request, res: Response<List[]>) {
         const { name } = req.params;
 
-        const board: list[] = [ { id: 1, name: "todo", items: [ { id: 1, name: "Get to work" }, { id: 2, name: "Pick up groceries" }, { id: 3, name: "Go home" }, { id: 4, name: "Fall asleep" } ] }, { id: 2, name: "busy", items: [ { id: 5, name: "Get up" }, { id: 6, name: "Brush teeth" }, { id: 7, name: "Take a shower" }, { id: 8, name: "Check e-mail" }, { id: 9, name: "Walk dog" } ] }, { id: 3, name: "done", items: [ { id: 10, name: "grad stuff" } ] }, { id: 4, name: "backlog", items: [ { id: 11, name: "db versioning" } ] } ]
+        const board: List[] = [ { id: 1, name: "todo", tickets: [ { id: 1, name: "Get to work" }, { id: 2, name: "Pick up groceries" }, { id: 3, name: "Go home" }, { id: 4, name: "Fall asleep" } ] }, { id: 2, name: "busy", tickets: [ { id: 5, name: "Get up" }, { id: 6, name: "Brush teeth" }, { id: 7, name: "Take a shower" }, { id: 8, name: "Check e-mail" }, { id: 9, name: "Walk dog" } ] }, { id: 3, name: "done", tickets: [ { id: 10, name: "grad stuff" } ] }, { id: 4, name: "backlog", tickets: [ { id: 11, name: "db versioning" } ] } ]
         res.send(board);
     }
 
