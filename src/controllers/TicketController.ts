c32232ed92d5e11e0c0b86790b1db618c34c65db
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-05-28 17:39:45 +0200

    Move ticket endpoint

--- src/controllers/TicketController.ts
+++ src/controllers/TicketController.ts
@@ -1,15 +1,17 @@
 import {Controller, Patch} from "../decorators";
-import {controller, EndpointDefenition, ErrorResponse, SuccesResponse} from "../interfaces";
+import {controller, EndpointDefenition, ErrorResponse, MyBoards, SuccesResponse} from "../interfaces";
 import {Request, Response} from "express";
 import {DBPool} from "../database";
 import {AssignTicketRequest} from "../interfaces/Requests/assignTicket";
+import {QueryResult} from "pg";
+import {MoveTicketRequest} from "../interfaces/Requests/moveTicket";
 
 @Controller('/ticket')
 export class TicketController implements controller {
-    // Define these to make the interface happy
+
     endpoint!: string;
     endpoints!: { [key: string]: EndpointDefenition; };
-    // define these to make the array in index.ts happy
+
     static endpoint = '';
     static endpoints = {}
 
@@ -51,5 +53,60 @@ export class TicketController implements controller {
             message: "User successfully assigned to ticket",
             code: 200
         })
+    }
+
+    @Patch('/move')
+    async moveTicket(req: Request<MoveTicketRequest>, res: Response<SuccesResponse| ErrorResponse>) {
+        const { moveToListId, ticketId } = req.body;
+        const userId = 3;
+        console.log(req.body);
+        const { rows } = await DBPool.query(`
+            WITH updated_tickets AS (
+                WITH cte AS (
+                    SELECT
+                        b."userId" AS board_owner,
+                        b."boardCollaborators",
+                        t."userId" AS ticket_owner,
+                        t."listId" AS current_list_id,
+                        l."boardId" AS board_id
+                    FROM
+                        "Boards" b
+                            JOIN "Lists" l ON b."boardId" = l."boardId"
+                            JOIN "Tickets" t ON l."listId" = t."listId"
+                    WHERE
+                        t."ticketId" = $1
+                      AND l."listId" = t."listId"
+                    )
+                    UPDATE "Tickets" t
+                        SET "listId" = $3
+                        WHERE
+                            t."ticketId" = $1
+                                AND EXISTS (
+                                SELECT 1
+                                FROM cte
+                                WHERE
+                                    ($2 = cte.board_owner OR $2 = ANY(cte."boardCollaborators"))
+                                  AND $3 IN (SELECT "listId" FROM "Lists" WHERE "boardId" = cte.board_id)
+                                  AND cte.current_list_id IN (SELECT "listId" FROM "Lists" WHERE "boardId" = cte.board_id)
+                            )
+                        RETURNING t.*
+            )
+            SELECT
+                CASE
+                    WHEN EXISTS (SELECT 1 FROM updated_tickets) THEN 'Success'
+                    ELSE 'Unauthorized'
+                    END AS status;
+        `, [ticketId, userId, moveToListId]);
+        if(rows[0].status === "Unauthorized") {
+            res.status(404).send({
+                message: "List not found",
+                code: 404
+            });
+            return;
+        }
+        res.send({
+            message: "List successfully updated",
+            code: 200
+        });
     }
 }
