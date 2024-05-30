acd622afe6249bf29a3ccdcc9494540b9dd47d9b
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-05-30 20:13:56 +0200

    edit board

--- src/controllers/BoardController.ts
+++ src/controllers/BoardController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
-import {Controller, Delete, Get, Post} from "../decorators";
+import {Controller, Delete, Get, Patch, Post} from "../decorators";
 import {Board, controller, EndpointDefenition, ErrorResponse, MyBoards} from '../interfaces';
 import { List } from "../interfaces/entities";
 import { DBPool } from '../database';
 import {QueryResult} from "pg";
 import {AddBoardRequest} from "../interfaces/Requests/addBoard";
+import {EditBoardRequest} from "../interfaces/Requests/editBoard";
 
 interface wrapper {
     board_data: Board
@@ -162,6 +163,35 @@ export class BoardController implements controller {
             res.send({ success: true, boardId: rows[0].boardId });
         } else {
             res.status(404).json({ error: 'Board not found' });
+        }
+    }
+
+    @Patch('/')
+    async editBoard(req: Request<EditBoardRequest>, res: Response) {
+        const { boardId, boardName, isPublic, boardCollaborators }: EditBoardRequest = req.body;
+        const userId = 3;  // Assuming userId is available in req.user
+        const { rows } = await DBPool.query(`
+            WITH authorized_user AS (
+                SELECT 1
+                FROM "Boards" b
+                WHERE b."boardId" = $1
+                  AND (b."userId" = $4 OR $4 = ANY(b."boardCollaborators"))
+                  AND b."isDeleted" = FALSE
+            )
+            UPDATE "Boards"
+            SET "boardName" = COALESCE($2, "boardName"),
+                "isPublic" = COALESCE($3, "isPublic"),
+                "boardCollaborators" = COALESCE($5::INTEGER[], "boardCollaborators")
+            WHERE "boardId" = $1
+              AND EXISTS (SELECT 1 FROM authorized_user)
+            RETURNING "boardId", "userId", "boardName", "isPublic", "boardCollaborators";
+        `, [boardId, boardName, isPublic, userId, boardCollaborators]);
+
+        if (rows.length > 0) {
+            const board = rows[0];
+            res.send(board);
+        } else {
+            res.status(404).send({ error: 'Board not found' });
         }
     }
 
