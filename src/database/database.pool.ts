df8327b65b0d0c170f9fa54c9545be583a88cdfb
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-06-03 21:37:25 +0200

    unnecesary log

--- src/database/database.pool.ts
+++ src/database/database.pool.ts
@@ -1,6 +1,5 @@
 import { Pool, PoolConfig } from 'pg';
 
-console.log(process.env.CONTAINER);
 const config = process.env.CONTAINER === 'true' ?
     {
         connectionString: process.env.DB_CONNECTION_STRING
